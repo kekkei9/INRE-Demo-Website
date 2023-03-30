d31de621bc40f66e252c3df5a9d8d07c87fcf869
trongnhandev1011 <kekkei2002@gmail.com>
2023-03-31 00:50:29 +0700

    fix build

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,8 +2,29 @@ import { Footer } from "@components/Footer";
 import { HeaderComponent } from "@components/Header";
 import Image from "next/image";
 import { useState } from "react";
-import { contentList, headerList } from "./headerList";
 import { NavData } from "@context/NavData";
+import { Nav } from "@src/types/nav";
+import { HomePage } from "@components/HomePage";
+import OfferingSection from "@components/HomePage/OfferingSection";
+import { TeamTab } from "@components/HomePage/TeamSection.tsx";
+import ContactSection from "@components/HomePage/ContactSection";
+import BlogSection from "@components/HomePage/BlogSection";
+
+export const headerList: Nav[] = [
+  { key: "home", label: "Home" },
+  { key: "offerings", label: "Our Offerings" },
+  { key: "team", label: "Our Team" },
+  { key: "contact", label: "Contact Us" },
+  { key: "blogs", label: "Blogs" },
+];
+
+export const contentList = {
+  home: <HomePage />,
+  offerings: <OfferingSection isInTab />,
+  team: <TeamTab />,
+  contact: <ContactSection />,
+  blogs: <BlogSection />,
+};
 
 export default function Home() {
   const [nav, setNav] = useState("home");
