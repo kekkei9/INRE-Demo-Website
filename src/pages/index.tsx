7579e60ea9c2fcd8aa7366831bf745159df77a51
trongnhandev1011 <kekkei2002@gmail.com>
2023-04-03 01:10:09 +0700

    fix build

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,5 @@
-import { Footer } from "@components/Footer";
-import { HeaderComponent } from "@components/Header";
-import Image from "next/image";
-import { useState } from "react";
-import { NavData } from "@context/NavData";
-import { Nav } from "@src/types/nav";
-import { HomePage } from "@components/HomePage";
 import OfferingSection from "@components/DisplaySections/OfferingSection/OfferingSection";
 import ContactSection from "@components/DisplaySections/ContactSection/ContactSection";
-import { TeamTab } from "@components/DisplaySections/TeamSection";
-import { BlogSection } from "@components/DisplaySections/BlogSection";
 import FirstSection from "@components/HomePage/FirstSection";
 import { BeginSection } from "@components/DisplaySections/BeginSection";
 import { PersonCardList } from "@components/CardLists/PersonCardList";
