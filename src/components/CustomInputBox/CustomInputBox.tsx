d2300bd152cd7abdb9a1d56faac440a9c1c0eff4
trongnhandev1011 <kekkei2002@gmail.com>
2023-04-10 22:02:45 +0700

    fix overlapped title

--- src/components/CustomInputBox/CustomInputBox.tsx
+++ src/components/CustomInputBox/CustomInputBox.tsx
@@ -7,7 +7,7 @@ const CustomInputBox = ({
 }: FormElem & { children: React.ReactNode }) => {
   return (
     <div className="font-medium w-[21.875rem] sm:w-[17.875rem] relative">
-      <div className="absolute bg-white left-6 top-[-0.84375rem] px-[0.375rem] z-10">
+      <div className="absolute bg-white left-6 top-[-0.84375rem] px-[0.375rem] z-[5]">
         {label}
       </div>
       {children}
