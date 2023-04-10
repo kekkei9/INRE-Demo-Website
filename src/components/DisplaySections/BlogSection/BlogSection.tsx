f4aabcede3c630f51c1d2ead6ad4dc0cc426c19d
trongnhandev1011 <kekkei2002@gmail.com>
2023-04-10 21:16:34 +0700

    change hard coded data -> fetch

--- src/components/DisplaySections/BlogSection/BlogSection.tsx
+++ src/components/DisplaySections/BlogSection/BlogSection.tsx
@@ -1,39 +1,48 @@
 import Image from "next/image";
-import { blogList } from "../../HomePage/ThirdSection";
 import { CardList } from "@components/CardLists/CardList";
 import { BlogCard } from "@components/CardLists/BlogCard";
+import { Blog } from "@src/types/blog";
+import useSWR from "swr";
 
-const BlogSection = () => (
-  <div className="pb-[3.4375rem]">
-    <div className="div-section bg-[#E6EAEE]">
-      <div className="bg-[#F2F4F6] px-[1.6875rem] py-[1.4375rem] flex gap-[1.875rem] items-center flex-col 2xl:flex-row">
-        <div className="flex-shrink-0">
-          <Image
-            src={"/assets/home/blogs/landing.png"}
-            alt="landing"
-            width={731}
-            height={373}
-          />
-        </div>
-        <div>
-          <div className="text-4xl font-bold">
-            The Most Expensive Bangalore Real Estate Sales For 2023
+const BlogSection = () => {
+  const { data: blogList } = useSWR<Blog[]>({
+    url: "/blogs",
+    args: {},
+  });
+
+  return (
+    <div className="pb-[3.4375rem]">
+      <div className="div-section bg-[#E6EAEE]">
+        <div className="bg-[#F2F4F6] px-[1.6875rem] py-[1.4375rem] flex gap-[1.875rem] items-center flex-col 2xl:flex-row">
+          <div className="flex-shrink-0">
+            <Image
+              src={"/assets/home/blogs/landing.png"}
+              alt="landing"
+              width={731}
+              height={373}
+            />
           </div>
-          <div className="text-2xl mt-[0.625rem] sm:mt-[2.1875rem]">
-            with several sales in the Bangalore eclipsing the Rs.200 cr mark. As
-            the pandemic continued, the ultra-wealthy sought second (and third)
-            homes to add to their portfolios and were willing to spend a fortune
-            to make their residences more comfortable for extended periods.
+          <div>
+            <div className="text-4xl font-bold">
+              The Most Expensive Bangalore Real Estate Sales For 2023
+            </div>
+            <div className="text-2xl mt-[0.625rem] sm:mt-[2.1875rem]">
+              with several sales in the Bangalore eclipsing the Rs.200 cr mark.
+              As the pandemic continued, the ultra-wealthy sought second (and
+              third) homes to add to their portfolios and were willing to spend
+              a fortune to make their residences more comfortable for extended
+              periods.
+            </div>
           </div>
         </div>
       </div>
+      <div className="div-section">
+        <CardList dataList={blogList}>
+          <BlogCard />
+        </CardList>
+      </div>
     </div>
-    <div className="div-section">
-      <CardList dataList={blogList}>
-        <BlogCard />
-      </CardList>
-    </div>
-  </div>
-);
+  );
+};
 
 export default BlogSection;
--- src/components/HomePage/ThirdSection.tsx
+++ src/components/HomePage/ThirdSection.tsx
@@ -2,52 +2,24 @@ import { BeginSection } from "@components/DisplaySections/BeginSection";
 import { Blog } from "@src/types/blog";
 import { CardList } from "@components/CardLists/CardList";
 import { BlogCard } from "@components/CardLists/BlogCard";
+import useSWR from "swr";
 
-export const blogList: Blog[] = [
-  {
-    id: "1",
-    imgSrc: "/assets/home/blogs/img_1.png",
-    address: "Real Estate, Analysis",
-    time: "09 jun 2022",
-    by: "admin",
-    title: "Application of Blockchain in real estate industry",
-    status: "buying",
-    description:
-      '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod temp incididuut labore dolore magna aliqua do eiusmod...',
-  },
-  {
-    id: "2",
-    imgSrc: "/assets/home/blogs/img_2.png",
-    address: "Real Estate, Analysis",
-    time: "09 jun 2022",
-    by: "admin",
-    title: "Real estate Challenges in Bengaluru",
-    status: "buying",
-    description:
-      '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod temp incididuut labore dolore magna aliqua do eiusmod...',
-  },
-  {
-    id: "3",
-    imgSrc: "/assets/home/blogs/img_3.png",
-    address: "Real Estate, Analysis",
-    time: "09 jun 2022",
-    by: "admin",
-    title: "Artificial Intelligence in Realty",
-    status: "buying",
-    description:
-      '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod temp incididuut labore dolore magna aliqua do eiusmod...',
-  },
-];
+const ThirdSection = () => {
+  const { data: blogList } = useSWR<Blog[]>({
+    url: "/blogs",
+    args: {},
+  });
 
-const ThirdSection = () => (
-  <div className="div-section bg-[#F1F1F1] sm:py-[3.4375rem]">
-    <div className="mb-9">
-      <BeginSection label="Blogs" />
+  return (
+    <div className="div-section bg-[#F1F1F1] sm:py-[3.4375rem]">
+      <div className="mb-9">
+        <BeginSection label="Blogs" />
+      </div>
+      <CardList dataList={blogList}>
+        <BlogCard />
+      </CardList>
     </div>
-    <CardList dataList={blogList}>
-      <BlogCard />
-    </CardList>
-  </div>
-);
+  );
+};
 
 export default ThirdSection;
